5925b665141964f63c2913d8eb0aff3137a80bf4
David <iis_dbga@live.com>
2022-12-09 18:03:28 -0600

    front page progress

--- mujeres-viajeras/mujeres-viajeras/src/Components/NavBar.jsx
+++ mujeres-viajeras/mujeres-viajeras/src/Components/NavBar.jsx
@@ -1,41 +1,49 @@
-import L from "../img/logoimg.png"
+import logo from "../img/1.png"
+
 
 const NavBar = () => {
     return ( <div>
+<nav className="navbar navbar-expand-lg navbar-light text-capitalize">
+    
+    <div className="container-xxl">
+        <a className="navbar-brand"><img src={logo} height="100px" alt="#" /></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#show-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
 
-<div className="contaner">
-    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom align-items-center">
-        <a href="localhost:3000/home" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <div className="bi me-2" width="20" height="20"> <img src={L} width="50px" height="50px"/> 
-            <span className="fs-4">Mujeres Viajeras</span>
-            </div>
-        </a>
-
-        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" >
-            <input type="search" className="form-control" placeholder="Busqueda..." aria-label="Search" />
-        </form>
-
-
-        <ul className="nav nav-pills align-items-center">
-            <li className="nav-item">
-                <a href="#" className="nav-link btn" aria-current="page">Home</a>
-            </li>
-
-            <li className="nav-item">
-                <a href="#" className="nav-link" aria-current="page">Blog</a>
-            </li>
-
-            <li className="nav-item">
-                <a href="#" className="nav-link" aria-current="page">Shop</a>
-            </li>
-
-            <li className="nav-item">
-                <a href="#" className="nav-link" aria-current="page">Profile</a>
-            </li>
-        </ul>
-    </header>
-
-</div>
+        <div className="collapse navbar-collapse" id="show-menu">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                    <a className="nav-link">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link">about</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link">Service</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link">travel agency</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link">contact us</a>
+                </li>
+                <li className="nav-item .search-container">
+                    <a className="nav-link search" ><i className="fas fa-search"></i></a>
+                    <form>
+                        <input type="search"/>
+                    </form>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" ><i className="far fa-user"></i></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" ><i className="far fa-heart"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
     </div> );
 }
