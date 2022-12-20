d8bd53942102803268cc5a39addec50991e7229c
David <iis_dbga@live.com>
2022-12-20 17:25:47 -0600

    create user form half way there

--- mujeres-viajeras/mujeres-viajeras/src/Components/Register.jsx
+++ mujeres-viajeras/mujeres-viajeras/src/Components/Register.jsx
@@ -1,4 +1,17 @@
 import { useState} from "react"
+import NavBar from './NavBar'
+import BG from "../img/woman.jpg"
+
+
+var sectionStyle = {
+    width: "100%",
+    height: "1041px",
+    backgroundImage: `url(${ BG } )`,
+    backgroundrepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  };
+
 
 const Register = () => {
     const [userName, setUserName] = useState('')
@@ -38,69 +51,108 @@ const Register = () => {
 
     }
     return ( 
-    <div>
-        <h1>Register</h1>
+    <div style={sectionStyle}>
+        <NavBar/>
+
+        <div className="container w-25 text-center bg-light ">
+        <h1>Registro</h1>
 
         <form onSubmit={registerUser}>
+            <div className="mb-1">
+            <label for='userName' className="form-label">Elige un apodo</label>
+            <br />
             <input 
             value={userName}
             onChange={(e)=> setUserName(e.target.value)}
             type="text" 
             placeholder="Nombre de usuario (apodo)"
-            >Apodo</input>
+            ></input>
+            </div>
+
+            <br />
+            
+            <div className="mb-1">
+            <label for="name" className="form-label">¿Como te llamas?</label>
             <br />
             <input 
             value={name}
             onChange={(e)=> setName(e.target.value)}
             type="text" 
             placeholder="Nombre"
-            >nombre completo:</input>
+            ></input>
+            </div>
+            <br />
+
+            <div className="mb-1">
+            <label for="email" className="form-label">¿Cual es tu mail?</label>
             <br />
             <input 
             value={email}
             onChange={(e)=> setEmail(e.target.value)}
             type="email" 
             placeholder="Email"
-            >Email:</input>
+            ></input>
+            </div>
+            <br />
+            
+            <div className="mb-1"></div>
+            <label for="password">Elige una contraseña:</label>
             <br />
             <input 
             value={password}
             onChange={(e)=> setPassword(e.target.value)}
             type="text" 
             placeholder="Contraseña"
-            >Constraseña</input>
+            ></input>
+            <br />
+            <div className="mb-1">
+            <label for="age">Fecha de nacimiento:</label>
             <br />
             <input 
             value={age}
             onChange={(e)=> setAge(e.target.value)}
             type="text" 
             placeholder="Edad"
-            >Edad:</input>
+            ></input>    
+            </div>
+            
+            
+            <br />
+
+            <div className="mb-1">
+            <label for="origin" value="">En que ciudad vives?</label>
             <br />
             <input 
             value={origin}
             onChange={(e)=> setOrigin(e.target.value)}
             type="text" 
             placeholder="Origen"
-            >origin</input>
+            ></input>
+            </div>
             <br />
+            <h5>¿Cual es tu número telefonico?</h5>
             <input 
             value={telephone}
             onChange={(e)=> setTelephone(e.target.value)}
             type="text" 
             placeholder="Telefono"
-            >telefono</input>
+            ></input>
             <br />
+            <h5>Elige una imagen de perfil</h5>
             <input 
             value={imgPic}
             onChange={(e)=> setImgPic(e.target.value)}
             type="text" 
             placeholder="imagen de perfil"
-            >Imagen de perfil</input>
+            ></input>
+
+            <input type="submit" value="upload" />
+            <br />
+            <input type="submit" value="register" className="mt-2"/>
         </form>
 
 
-
+        </div>
     </div> );
 }
  
